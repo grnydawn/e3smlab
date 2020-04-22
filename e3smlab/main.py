6b4ff999ddfbc9dadc48896bec7a11671ff0939c
Youngsung Kim <youngsung.kim.act2@gmail.com>
2020-04-22 12:44:20 -0400

    requiree meteolab>=0.1.3

--- e3smlab/main.py
+++ e3smlab/main.py
@@ -4,7 +4,7 @@ from .e3smtimestat import E3SMTimeStat
 
 class E3SMLab(Project):
     _name_ = "e3smlab"
-    _version_ = "0.1.2"
+    _version_ = "0.1.3"
     _description_ = "E3SM Analysis Utilities"
     _long_description_ = "Tools for Analysis of E3SM project"
     _author_ = "Youngsung Kim"
