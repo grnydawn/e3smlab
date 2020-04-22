b3fd986d2f04310a191de14a1fb571570f5442ec
Youngsung Kim <youngsung.kim.act2@gmail.com>
2020-04-22 12:00:27 -0400

    fixed a typo

--- e3smlab/__main__.py
+++ e3smlab/__main__.py
@@ -1,8 +1,8 @@
 """main entry for e3smlab command-line interface"""
 
 def main():
-    from e3smlab import E3SMlab
-    return E3SMlab().main()
+    from e3smlab import E3SMLab
+    return E3SMLab().main()
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 def main():
 
     from setuptools import setup
-    from e3smlab.main import E3SMlab as elab
+    from e3smlab.main import E3SMLab as elab
 
     console_scripts = ["e3smlab=e3smlab.__main__:main"]
     install_requires = ["meteolab>=0.1.0", "gunzip", "nmlread", "dict2json"]
